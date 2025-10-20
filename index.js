c90741a19cdfc87f2bf4c9b5525dc68c718791e7
Henry Macfarlane <62835889+GRANDMAC96@users.noreply.github.com>
2025-10-20 17:10:06 +0100

    ADDED JS FUNCTIONALITY TO RESUME CONTAINER
    Added some js to apply to the resume container
    meaning that when the mouse hovers over it the
    hover colours apply to the parent and child
    container.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
 
-document.getElementById("flip-card").addEventListener("click", function() {
-    this.classList.toggle("flipped");
-    console.log("hello");
-  });
+document.getElementById("flip-card").addEventListener("click", function () {
+  this.classList.toggle("flipped");
+  console.log("hello");
+});
 
+const innerbox = document.getElementById("resumetext");
+const box = document.getElementById("resume");
+
+
+box.addEventListener("mouseenter", () => {
+  innerbox.style.backgroundColor = "#e0e0ff";
+});
+
+box.addEventListener("mouseleave", () => {
+  innerbox.style.backgroundColor = "rgb(107, 107, 109)";
+});
